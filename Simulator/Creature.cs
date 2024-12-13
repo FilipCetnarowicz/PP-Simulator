e18a1a7ab3b57163932f797345ad1e2603a81ae1
FilipCetnarowicz <fcetnarowicz@gmail.com>
2024-12-13 22:07:28 +0100

    Stwory

---
+++ Simulator/Creature.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simulator;
+public class Creature
+{
+    // fields
+    public required string Name { get; init; }
+    public int Level { get; set; }
+    public string Info => $"{Name}, [{Level}]";
+
+    // constructors
+    public Creature (string name, int level = 1)
+    {
+        Name = name; 
+        Level = level;
+    }
+    public Creature() { }
+
+    //methods
+    public void SayHi() =>
+        Console.WriteLine($"Hi, I'm {Name}, my level is {Level}.";
+
+}
