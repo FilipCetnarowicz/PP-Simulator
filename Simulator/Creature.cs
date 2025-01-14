70991dc595afa2cfed39fe9b2b953075e9b86df9
FilipCetnarowicz <fcetnarowicz@gmail.com>
2025-01-14 20:29:00 +0100

    Let's go

--- Simulator/Creature.cs
+++ Simulator/Creature.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Simulator.Maps;
 namespace Simulator;
 public abstract class Creature
 {
@@ -23,8 +24,10 @@ public abstract class Creature
     }
     
     public abstract int Power { get; }
-
     public abstract string Info { get; }
+
+    public SmallMap? CurrentMap { get; private set; }
+    public Point? CurrentPosition { get; private set; }
 
     // constructors
     public Creature(string name, int level = 1)
@@ -40,12 +43,24 @@ public abstract class Creature
     public void Upgrade() =>
         level = level < 10 ? level + 1 : level;
 
-    public string Go(Direction direction) =>
-        $"{Name}[{Level}] goes {direction.ToString().ToLower()}.";
-    public string[] Go(List<Direction> directions) =>
-        directions.Select(Go).ToArray();
-    public string[] Go(string directionsToString) =>
-        Go(DirectionParser.Parse(directionsToString));
+    public void Go(Direction direction)
+    {
+        if (CurrentMap == null || CurrentPosition == null) 
+            throw new InvalidOperationException("Creature isn't assigned yet. Use .AssignToMap()");
+        var newPosition = CurrentPosition.Value.Next(direction);
+        CurrentMap.Move(this, newPosition);
+        CurrentPosition = newPosition;
+    }
+
+    public void AssignToMap(SmallMap map, Point position)
+    {
+        if (CurrentMap != null) throw new InvalidOperationException("Creature is already assigned to map.");
+        CurrentMap = map;
+        CurrentPosition = position;
+        map.Add(this, position);
+    }
+    public void AssignToMap(SmallMap map, int x, int y)
+        { AssignToMap(map, new Point(x, y)); }
 
     public override string ToString()
         { return $"{GetType().Name.ToUpper()}: {Info}"; }
--- Simulator/Maps/SmallMap.cs
+++ Simulator/Maps/SmallMap.cs
@@ -1,9 +1,58 @@
 ﻿namespace Simulator.Maps;
 public abstract class SmallMap : Map
 {
-    public SmallMap(int sizeX, int sizeY) : base(sizeX, sizeY) 
-    { 
+    public Dictionary<Point, List<Creature>> Participants = new();
+    public SmallMap(int sizeX, int sizeY) : base(sizeX, sizeY)
+    {
         if (sizeX > 20 || sizeY > 20)
             { throw new ArgumentOutOfRangeException("maximum size - 20x20"); }
     }
+
+    public void Add(Creature creature, Point position)
+    {
+        if (!this.Exist(position)) 
+            throw new ArgumentOutOfRangeException("Invalid point. Check map size");
+        if (!Participants.ContainsKey(position))
+            Participants[position] = new List<Creature>();
+        Participants[position].Add(creature);
+    }
+
+    public void Remove(Creature creature, Point position)
+    {
+        if (!this.Exist(position))
+            throw new ArgumentOutOfRangeException("Invalid point. Check map size");
+        if (!Participants.ContainsKey(position) || !Participants[position].Contains(creature)) 
+            throw new ArgumentException("Your creature is elsewhere.");
+        Participants[position].Remove(creature);
+        if (Participants[position].Count == 0)
+            Participants.Remove(position);
+    }
+
+    public void Move(Creature creature, Point newPosition)
+    {
+        if (!this.Exist(newPosition))
+            throw new ArgumentOutOfRangeException("Invalid point. Check map size");
+        if (creature.CurrentPosition == null)
+            throw new ArgumentException("This participant isn't on the map. Place him first.");
+        var currentPosition = creature.CurrentPosition.Value;
+        Participants[currentPosition].Remove(creature);
+        if (Participants[currentPosition].Count==0)
+            Participants.Remove(currentPosition);
+        if (!Participants.ContainsKey(newPosition))
+            Participants[newPosition] = new List<Creature>();
+        Participants[newPosition].Add(creature);
+    }
+
+    public List<Creature> At(Point position)
+    {
+        if (!this.Exist(position)) 
+            throw new ArgumentOutOfRangeException("Invalid point. Check map size.");
+        if (!Participants.ContainsKey(position))
+            throw new ArgumentException("There is no participant on this position.");
+        return new List<Creature>(Participants[position]);
+    }
+
+    public List<Creature> At(int x, int y)
+        { return At(new Point(x,y)); }
+
 }
